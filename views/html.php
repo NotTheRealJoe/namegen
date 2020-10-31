c05e5012be9cc91c3596774013d51dfb957a675d
Nigel Ticknor <nigelticknor@gmail.com>
2020-10-31 21:03:46 +0000

    accept more mime types

---
+++ views/html.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html>
+<body style="text-align: center">
+	<h2>Your new name is...</h2>
+	<h1><?php echo $new_name; ?></h1>
+	<span style="font-size: 0.7em; color: #404040">Copyright &copy; 2018 Joe Tortorello. Uses components from Princeton University WordNet&reg;. <a style="color:inherit" href="http://wordnet.princeton.edu">http://wordnet.princeton.edu</a></span> 
+</body>
+</html>
